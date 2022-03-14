3468d72d1233f4dabdd16d253957735c9b57a03d
Fernando Papito <p4p1to@outlook.com>
2022-03-14 18:28:18 -0300

    desafio da página de login

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,18 @@
+
+// Cenários:
+
+// 1 - Login com sucesso
+// 2 - Senha incorreta
+// 3 - Email no formato inválido
+// 4 - Campos obrigatórios (dois ITs)
+
+// Dicas
+
+// 1 - Para fazer login, é necessário já ter uma conta cadastrada
+// 2 - Usar contextos
+// 3 - Para fazer o login com sucesso, você deverá componentizar alguma coisa
+
+// Como entregar
+
+// Publicar o no repositório do Github
+// Em seguida, colocar o link aqui em abaixo na sessão de comentários
