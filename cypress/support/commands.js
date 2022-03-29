7fae20a431b86897158bf01beb71c5dcc83d7879
Fernando Papito <p4p1to@outlook.com>
2022-03-29 18:31:50 -0300

    Ajustes importantes (Guide Line)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,7 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
+import moment from 'moment'
 
 Cypress.Commands.add('postUser', function (user) {
     cy.task('removeUser', user.email)
@@ -58,3 +58,72 @@ Cypress.Commands.add('recoveryPass', function (email) {
     })
 
 })
+
+Cypress.Commands.add('createAppointment', function(hour){
+    let now = new Date()
+    now.setDate(now.getDate() + 1)
+
+    Cypress.env('appointmentDay', now.getDate())
+
+    const date = moment(now).format('YYYY-MM-DD ' + hour + ':00')
+    
+    const payload = {
+        provider_id: Cypress.env('providerId'),
+        date: date
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/appointments',
+        body: payload,
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+    }).then(function(response){
+        expect(response.status).to.eq(200)
+    })
+
+})
+
+Cypress.Commands.add('setProviderId', function(providerEmail){
+
+    cy.request({
+        method: 'GET',
+        url: 'http://localhost:3333/providers',
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+    }).then(function(response){
+        expect(response.status).to.eq(200)
+        console.log(response.body)
+
+        const providerList = response.body
+
+        providerList.forEach(function(provider){
+            if (provider.email === providerEmail) {
+                Cypress.env('providerId', provider.id)
+            }
+        })
+
+    })
+
+})
+
+Cypress.Commands.add('apiLogin', function(user){
+
+    const payload = {
+        email: user.email,
+        password: user.password
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/sessions',
+        body: payload
+    }).then(function(response){
+        expect(response.status).to.eq(200)
+        Cypress.env('apiToken', response.body.token)
+    })
+
+})
+
