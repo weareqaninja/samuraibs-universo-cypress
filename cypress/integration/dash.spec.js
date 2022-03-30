8555a38be69d2601bb0b913b9b0cb2db57d6d75c
Fernando Papito <p4p1to@outlook.com>
2022-03-30 13:14:54 -0300

    Fixture? Que tal usar Factories em Javascript?

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -2,39 +2,25 @@
 import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
 
-describe('dashboard', function(){
+import { customer, provider, appointment } from '../support/factories/dash'
 
-    context('quando o cliente faz um agendamento no app mobile', function(){
+describe('dashboard', function () {
 
-        const data = {
-            customer: {
-                name: 'Nikki Sixx',
-                email: 'sixx@motleycrue.com',
-                password: 'pwd123',
-                is_provider: false
-            },
-            provider: {
-                name: 'Ramon Valdes',
-                email: 'ramon@televisa.com',
-                password: 'pwd123',
-                is_provider: true
-            },
-            appointmentHour: '14:00'
-        }
+    context('quando o cliente faz um agendamento no app mobile', function () {
 
-        before(function(){
-            cy.postUser(data.provider)
-            cy.postUser(data.customer)
-            
-            cy.apiLogin(data.customer)
-            cy.setProviderId(data.provider.email)
-            cy.createAppointment(data.appointmentHour)           
+        before(function () {
+            cy.postUser(provider)
+            cy.postUser(customer)
+
+            cy.apiLogin(customer)
+            cy.setProviderId(provider.email)
+            cy.createAppointment(appointment.hour)
         })
 
-        it('o mesmo deve ser exibido no dashboard', function(){
-            
+        it('o mesmo deve ser exibido no dashboard', function () {
+
             loginPage.go()
-            loginPage.form(data.provider)
+            loginPage.form(provider)
             loginPage.submit()
 
             dashPage.calendarShoudlBeVisible()
@@ -42,7 +28,7 @@ describe('dashboard', function(){
             const day = Cypress.env('appointmentDay')
             dashPage.selectDay(day)
 
-            dashPage.appointmentShouldBe(data.customer, data.appointmentHour)
+            dashPage.appointmentShouldBe(customer, appointment.hour)
         })
 
     })
---
+++ cypress/support/factories/dash/index.js
@@ -0,0 +1,18 @@
+
+exports.customer = {
+    name: 'Nikki Sixx',
+    email: 'sixx@motleycrue.com',
+    password: 'pwd123',
+    is_provider: false
+}
+
+exports.provider = {
+    name: 'Ramon Valdes',
+    email: 'ramon@televisa.com',
+    password: 'pwd123',
+    is_provider: true
+}
+
+exports.appointment = {
+    hour: '14:00'
+}
