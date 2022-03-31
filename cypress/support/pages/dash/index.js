c00f30ede8995f7a586ff5c974690d3982b81cb7
Fernando Papito <p4p1to@outlook.com>
2022-03-31 10:05:28 -0300

    Situações inusitadas ao trabalhar com datas

--- cypress/support/pages/dash/index.js
+++ cypress/support/pages/dash/index.js
@@ -14,6 +14,28 @@ class DashPage {
     }
 
     selectDay(day) {
+
+        let today = new Date()
+        let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0)
+
+        if(today.getDate() === lastDayOfMonth.getDate()) {
+            cy.log('Hoje é último dia do mês')
+
+            cy.get(el.nextMonthButton)
+                .should('be.visible')
+                .click()
+
+            // Isso é um checkpoint para garantir que houve a troca do calendário
+            cy.contains(el.monthYearName, 'Abril') 
+                .should('be.visible')
+
+        } else {
+            cy.log('Hoje não é o último dia do mês')
+        }
+
+        cy.log(today.toString())
+        cy.log(lastDayOfMonth.toString())
+
         const target = new RegExp('^' + day + '$', 'g')
         cy.contains(el.boxDay, target)
             .click({ force: true })
