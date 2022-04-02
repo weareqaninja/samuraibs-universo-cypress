49372a2bb966da7efc4b88c9e10647b0ea3aa02a
Fernando Papito <p4p1to@outlook.com>
2022-04-02 16:19:43 -0300

    Pageload e Checkpoints

--- cypress/support/pages/login/elements.js
+++ cypress/support/pages/login/elements.js
@@ -1,4 +1,5 @@
 exports.el = {
+    title: ('h1', 'Faça seu login'),
     email: 'input[placeholder$=email]',
     password: 'input[placeholder*=senha]',
     signIn: ('button[type=submit]', 'Entrar'),
--- cypress/support/pages/signup/index.js
+++ cypress/support/pages/signup/index.js
@@ -13,6 +13,9 @@ class SignupPage {
 
     go() {
         cy.visit('/signup')
+
+        cy.contains(el.title)
+            .should('be.visible')
     }
 
     form(user) {
