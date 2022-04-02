6ffd839d95fe95e1a256106a97b9f830315fd593
Fernando Papito <p4p1to@outlook.com>
2022-04-02 12:01:31 -0300

    screenshots e validações atraves de propriedades do CSS

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -5,6 +5,7 @@ class Toast {
     shouldHaveText(expectText) {
         cy.get(el.toast, { timeout: 10000 })
             .should('be.visible')
+            .should('have.css', 'opacity', '1', { timeout: 1500 })
             .find('p')
             .should('have.text', expectText)
     }
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,8 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// Screenshots para todos os testes (passando ou falhando)
+// afterEach(function(){
+//     cy.screenshot()
+// })
